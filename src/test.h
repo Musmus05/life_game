1068eb9c32700ece068ee40e39e8003fe30d5945
cherubalex <alexandresdh@gmail.com>
2024-12-06 16:24:33 +0100

    commit

--- src/test.h
+++ src/test.h
@@ -607,23 +607,23 @@ void test_grille_update_torique()
 
 
 void test (){
-    // test_lecture_fichier();
-    // test_ecriture_fichier();
-    // test_creer_dossier();
-    // test_supprimer_dossier();
-    // test_get_nom();
-    // test_grille_creation();
-    // test_set_ligne();
-    // test_set_colonne();
-    // test_set_matrice();
-    // test_get_matrice();
-    // test_cellule_creation_par_defaut();
-    // test_cellule_creation_par_parametres();
-    // test_getters();
-    // test_status_update();
-    // test_affichage_terminal_et_compteurs();
-    // test_affichage_graphique_et_compteurs();
-    // test_grille_update_default();
+    test_lecture_fichier();
+    test_ecriture_fichier();
+    test_creer_dossier();
+    test_supprimer_dossier();
+    test_get_nom();
+    test_grille_creation();
+    test_set_ligne();
+    test_set_colonne();
+    test_set_matrice();
+    test_get_matrice();
+    test_cellule_creation_par_defaut();
+    test_cellule_creation_par_parametres();
+    test_getters();
+    test_status_update();
+    test_affichage_terminal_et_compteurs();
+    test_affichage_graphique_et_compteurs();
+    test_grille_update_default();
     test_grille_update_torique();
 
 }
