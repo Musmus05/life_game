d2811690e3e0fbfacd6d8b48c6256baa6171ba60
Musmus05 <mustaphamadani43@gmail.com>
2024-12-06 15:50:15 +0100

    simplification des tests

--- src/main.cpp
+++ src/main.cpp
@@ -5,35 +5,18 @@ using namespace std;
 
 int main()
 {
-    int oui = 0;
+    int choix = 0;
     cout << "Voulez vous faire des tests unitaires 0 = non, 1 = oui ? " << endl;
-    cin >> oui;
-    if (oui == 0)
+    cin >> choix;
+    if (choix == 0)
     {
         Jeux_vie jeux_vie;
         jeux_vie.run();
     }
 
-    else if (oui == 1)
+    else if (choix == 1)
     {
-        test_creer_dossier();
-        test_lecture_fichier();
-        test_ecriture_fichier();
-        test_supprimer_dossier();
-        test_get_nom();
-        test_grille_creation();
-        test_set_ligne();
-        test_set_colonne();
-        test_set_matrice();
-        test_get_matrice();
-        test_cellule_creation_par_defaut();
-        test_cellule_creation_par_parametres();
-        test_getters();
-        test_status_update();
-        test_affichage_terminal_et_compteurs();
-        test_affichage_graphique_et_compteurs();
-        test_grille_update_default();
-        test_grille_update_torique();
+    test();
     }
 
     return 0;
